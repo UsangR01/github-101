433269e5ccc83dd2f2d89b36cee9a70e4d2922b1
Richard Usang <usangr01@gmail.com>
2024-04-03 22:54:03 +0100

    added name parameter

--- second_file.py
+++ second_file.py
@@ -2,7 +2,7 @@
 def print_hello_world():
     print("Hello World!")
 
-def print_my_name():
-    print("Hi! My name is Richard")
+def print_my_name(name):
+    print(f"Hi! My name is {name}!")
 
 print_hello_world()
